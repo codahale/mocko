6ca1acc31b15e68f8c2228e73e22268171291bf5
Coda Hale <coda.hale@gmail.com>
2016-02-20 17:08:12 -0800

    Use clojars.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/codahale/mocko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
