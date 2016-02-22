5ec62a0dd1ed0747112e4fb4059ba2d864c7f876
Coda Hale <coda.hale@gmail.com>
2016-02-22 14:52:35 -0800

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mocko "0.1.1"
+(defproject mocko "0.1.2-SNAPSHOT"
   :description "A simple mocking library."
   :url "https://github.com/codahale/mocko"
   :license {:name "Eclipse Public License"
