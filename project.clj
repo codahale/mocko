3ea645257464ad2f5a772652496201a70302f2b6
Coda Hale <coda.hale@gmail.com>
2017-02-03 19:57:05 -0800

    Upgrade combinatorics.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.3"]]
+                 [org.clojure/math.combinatorics "0.1.4"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :profiles {:dev [:project/dev :profiles/dev]
